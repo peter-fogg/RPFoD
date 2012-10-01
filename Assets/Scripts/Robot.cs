2bde64c516ebe503e3d601c7fdcef6d1e529f940
peter-fogg <peter.fogg@oberlin.edu>
2012-10-01 14:50:04 -0400

    added Robot.cs

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class Foo : MonoBehaviour {
+public class Robot : MonoBehaviour {
 
   // Use this for initialization
   void Start () {
@@ -10,6 +10,6 @@ public class Foo : MonoBehaviour {
 	
   // Update is called once per frame
   void Update () {
-    print("FOO!");
+	
   }
 }
