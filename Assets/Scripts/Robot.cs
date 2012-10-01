8101d8b4e3a4cfcbdab4d6bc931b40b238c19b87
peter-fogg <peter.fogg@oberlin.edu>
2012-10-01 14:54:32 -0400

    added Color enum

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -2,10 +2,15 @@ using UnityEngine;
 using System.Collections;
 
 public class Robot : MonoBehaviour {
-
+  
+  public int health;
+  public float speed;
+  public int damage;
+  
+  
   // Use this for initialization
   void Start () {
-	
+    
   }
 	
   // Update is called once per frame
