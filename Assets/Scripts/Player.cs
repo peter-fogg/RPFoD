cda8f5ffeae3253f3436b6e7d21189fae00532c2
oshoham <oshoham@oberlin.edu>
2012-10-07 13:22:24 -0400

    The player now detects for wall collisions and can't move through walls.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,12 @@ public class Player : MonoBehaviour {
 	public Color colorPainted; // the color that the player is currently painted
 	public Color colorShooting; // the color that the player is currently shooting
 
+
+	private Vector3 forward = new Vector3(0, 0, 0.5F);
+	private Vector3 backward = new Vector3(0, 0, -0.5F);
+	private Vector3 left = new Vector3(-0.5F, 0, 0);
+	private Vector3 right = new Vector3(0.5F, 0, 0);
+
 	// Use this for initialization
 	void Start () {
 		collider.enabled = true;
@@ -17,16 +23,20 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyDown("w") && transform.position.z < 4.5F) {
-			transform.Translate(new Vector3(0, 0, 0.5F));
+			if(!GameManager.CheckPosition(transform.position + forward))
+				transform.Translate(forward);
 		}	
 		if(Input.GetKeyDown("a") && transform.position.x > -4.5F) {
-			transform.Translate(new Vector3(-0.5F, 0, 0));
+			if(!GameManager.CheckPosition(transform.position + left))
+				transform.Translate(left);
 		}
 		if(Input.GetKeyDown("d") && transform.position.x < 4.5F) {
-			transform.Translate(new Vector3(0.5F, 0, 0));
+			if(!GameManager.CheckPosition(transform.position + right))
+				transform.Translate(right);
 		}
 		if(Input.GetKeyDown("s") && transform.position.z > -4.5F) {
-			transform.Translate(new Vector3(0, 0, -0.5F));
+			if(!GameManager.CheckPosition(transform.position + backward))
+				transform.Translate(backward);
 		}
 		//Debug.Log(transform.position);
 	}
