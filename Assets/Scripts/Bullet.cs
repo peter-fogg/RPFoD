9f666dde4de2d21213aa55d9c53d2de5dbe7bc02
peter-fogg <peter.fogg@oberlin.edu>
2012-10-10 20:16:08 -0400

    bullets can hit robots. also, robots are no longer suicidal. SSRIs work wonders

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,6 +6,7 @@ public class Bullet : MonoBehaviour {
 	public int damage;
 	public Vector3 direction;
 	public float speed;
+	public GameObject cameFrom;
 	
 	// Use this for initialization
 	void Start () {
@@ -21,7 +22,8 @@ public class Bullet : MonoBehaviour {
 		transform.Translate(direction*speed);
 	}
 
-	public static GameObject MakeBullet(Vector3 position, Vector3 direction, int damage, float speed) {
+	public static GameObject MakeBullet(Vector3 position, Vector3 direction, int damage,
+					    float speed, GameObject cameFrom) {
 		GameObject bullet = GameObject.CreatePrimitive(PrimitiveType.Cube);
 		bullet.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
 		bullet.transform.position = position;
@@ -31,16 +33,25 @@ public class Bullet : MonoBehaviour {
 		bulletScript.direction = direction;
 		bulletScript.damage = damage;
 		bulletScript.speed = speed;
+		bulletScript.cameFrom = cameFrom;
 		return bullet;
 	}
 
 	public void OnTriggerEnter(Collider other) {
+		if(other.gameObject == cameFrom) {
+			return;
+		}
 		Player player = other.gameObject.GetComponent<Player>();
-		print("blasdfsadf");
 		if(player != null) {
-			print("Dadgummit!");
 			player.health -= damage;
 			Destroy(gameObject);
+			return;
+		}
+		Robot robot = other.gameObject.GetComponent<Robot>();
+		if(robot != null) {
+			robot.health -= damage;
+			Destroy(gameObject);
+			return;
 		}
 	}
 }
