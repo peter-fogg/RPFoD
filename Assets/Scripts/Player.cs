71c5c148b2bb4bab9ddb94bf1467b0d24cc2b920
oshoham <oshoham@oberlin.edu>
2012-10-10 20:13:06 -0400

    The player can't shoot without having a color.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -165,7 +165,7 @@ public class Player : MonoBehaviour {
 			}
 		}
 	
-		if(Input.GetKeyDown(KeyCode.Space)) {
+		if(Input.GetKeyDown(KeyCode.Space) && (colorShooting == Color.red || colorShooting == Color.green || colorShooting == Color.blue)) {
 		// fires projectiles
 
 			Projectile.MakeProj(transform.position, dir, colorShooting);
